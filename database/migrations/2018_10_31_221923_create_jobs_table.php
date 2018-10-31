9a30a704aeb61418f677b5d3dbb9c8a84a3aed1a
am-chris <chris@automashion.com>
2018-10-31 15:43:17 -0700

    Add basic job functionality (#2)

---
+++ database/migrations/2018_10_31_221923_create_jobs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('name');
+            $table->string('slug');
+            $table->text('description');
+            $table->boolean('visible')->default(false);
+            $table->boolean('featured')->default(false);
+            $table->timestamps();
+            
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
